e306fae77edd74a55a01242e0eebcc0105f56a9a
t-skoro <65133778+t-skoro@users.noreply.github.com>
2021-10-12 01:08:48 +0500

    Добавлена мобильная адаптация

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 const schemeSvg = document.querySelector(".scheme-svg");
 const totalPriceTag = document.querySelector(".price-total");
+const menuButton = document.querySelector(".m-menu");
+const menu = document.querySelector(".menu");
 let cost = 300;
 let totalPrice = 0;
 schemeSvg.addEventListener("click", (event) => {
@@ -9,4 +11,7 @@ schemeSvg.addEventListener("click", (event) => {
         totalPrice = totalSeats * cost;
         totalPriceTag.textContent = totalPrice;
     }
+});
+menuButton.addEventListener("click", () => {
+    menu.classList.toggle("is-open");
 });
